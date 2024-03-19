a92b165091ed671a77661d494214a8d365aba955
Cristhian Peverelli <cristhian.peverelli@studenti.unimi.it>
2024-03-19 15:45:28 +0100

    adjustment

---
+++ assets/WorkSection.432771c7.js
@@ -0,0 +1,1 @@
+import{Q as o}from"./QPage.6901ec79.js";import{F as t,G as s,I as r,S as e}from"./index.8375df3f.js";import"./render.f5b17761.js";const a=e("h1",null,"Work Section",-1),c=e("h1",null,"Work in progress",-1),f=Object.assign({name:"WorkSection"},{__name:"WorkSection",setup(n){return(_,i)=>(t(),s(o,{class:"flex flex-center"},{default:r(()=>[a,c]),_:1}))}});export{f as default};
